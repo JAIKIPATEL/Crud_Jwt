c1b7644cc7582ef1ced3ed3a49a5345569932a7e
JAIKIPATEL <jaikipatel@gmail.com>
2022-08-18 10:34:20 +0530

    edit

--- config/db.config.js
+++ config/db.config.js
@@ -35,8 +35,6 @@
 // });
 
 // module.exports = pool;
-
-
 const mysql = require('mysql');
 
 // connection pool
--- server.js
+++ server.js
@@ -1,56 +1,3 @@
-// const bodyParser = require('body-parser');
-// const express = require('express');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
-// const error  = require('http-errors');
-// //const logger = require('logger');
-// const flash = require('express-flash');
-// const session = require('express-session');
-
-// // create express app
-// const app = express();
-// dotenv.config();
-
-// // // setup server port
-// const port = process.env.APP_PORT;
-
-// //routes path
-// const userRoute = require('./src/user/routes/user.routes');
-// //const postRoute = require('./src/post/routes/post.routes');
-
-
-// //parse request of content type app/urlencoded
-// app.use(bodyParser.urlencoded({ extended : true}));
-
-// //parse request of content-type app/json
-// app.use(bodyParser.json());
-
-// // using middleware - create routes
-// app.use(cors());
-// //app.use(logger('dev'));
-// app.use(flash());
-// app.use(session({
-//     secret: '123458cat',
-//     resave: false,
-//     saveUninitialized : true,
-//     cookie : {maxAge:60000}
-// }));
-
-// app.use('/api/user',userRoute);
-// //app.use('/api/post',postRoute);
-
-
-// app.get('/',(req,res) => {
-//     res.send("hello");
-// });
-
-// //listening the port
-// app.listen(process.env.APP_PORT, () => {
-//     console.log(`server is running on port ${port}`);
-
-// });
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -74,7 +21,7 @@ const profileRoute = require('./src/user/routes/profile.routes');
 
 
 // parse request of content-type -application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse request of content-type - application/json
 app.use(bodyParser.json());
@@ -85,10 +32,10 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(flash());
 app.use(session({
-    secret : '123458cat',
-    resave : false,
-    saveUninitialized : true,
-    cookie : {maxAge : 60000}
+    secret: '123458cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
 }));
 
 app.use('/api/user', userRoute);
